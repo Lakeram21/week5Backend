3f662e683a357ec4be3d75605208720b1ea8acaf
Lakeram Narine <nar19002@byui.edu>
2022-05-18 12:59:24 -0600

    added cors

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods','GET', 'POST')
     next();
   })
   .use('/', require('./routes'));
